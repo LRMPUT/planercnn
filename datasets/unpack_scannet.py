c88596f3e6e4fbd7a03a06c4dac72f75fb40b68f
jwietrzykowski <jan.m.wietrzykowski@gmail.com>
2020-02-05 11:47:10 +0100

    Extracting num frames.

--- datasets/unpack_scannet.py
+++ datasets/unpack_scannet.py
@@ -35,14 +35,18 @@ def main():
     if args.verbose:
         print("Reading scene_id file: " + args.inputdir)
 
-    scene_set = set()
-    scene_id_to_valid_idxs = {}
-    read_unpack_file(args.inputdir, 'train', scene_set, scene_id_to_valid_idxs)
-    read_unpack_file(args.inputdir, 'test', scene_set, scene_id_to_valid_idxs)
+    scenes_list = sorted(os.listdir(os.path.join(args.inputdir, 'scans')))
 
-    for scene_id, valid_idxs in scene_id_to_valid_idxs.items():
+    # scene_set = set()
+    # scene_id_to_valid_idxs = {}
+    # read_unpack_file(args.inputdir, 'train', scene_set, scene_id_to_valid_idxs)
+    # read_unpack_file(args.inputdir, 'test', scene_set, scene_id_to_valid_idxs)
+
+    # for scene_id, valid_idxs in scene_id_to_valid_idxs.items():
+    for scene_id in scenes_list:
         if not os.path.exists(os.path.join(args.inputdir, 'scans', scene_id, 'frames')):
             print('unpacking ' + 'scans/' + scene_id)
+            # pass
 
             # python extractor
             # print(' '.join(['executing:', 'python',
@@ -63,38 +67,50 @@ def main():
             #                          '--export_poses',
             #                          '--export_intrinsics'],
             #                         cwd=os.path.join(args.inputdir, 'scans', scene_id))
-
-            # C++ extractor
-            print(' '.join(['executing:',
-                            args.scriptpath,
-                            scene_id + '.sens',
-                            'frames']))
+            print(' '.join(['executing:', 'python',
+                                     args.scriptpath,
+                                     '--filename=scans/' + scene_id + '.sens',
+                                     '--output_path=.',
+                                     '--export_num_frames']))
             print('in scene_id: ' + os.path.join(args.inputdir, 'scans', scene_id))
-            proc = subprocess.Popen([args.scriptpath,
-                                     scene_id + '.sens',
-                                     'frames'],
+            proc = subprocess.Popen(['python',
+                                     args.scriptpath,
+                                     '--filename=scans/' + scene_id + '.sens',
+                                     '--output_path=.',
+                                     '--export_num_frames'],
                                     cwd=os.path.join(args.inputdir, 'scans', scene_id))
+
+            # # C++ extractor
+            # print(' '.join(['executing:',
+            #                 args.scriptpath,
+            #                 scene_id + '.sens',
+            #                 'frames']))
+            # print('in scene_id: ' + os.path.join(args.inputdir, 'scans', scene_id))
+            # proc = subprocess.Popen([args.scriptpath,
+            #                          scene_id + '.sens',
+            #                          'frames'],
+            #                         cwd=os.path.join(args.inputdir, 'scans', scene_id))
             stdout, stderr = proc.communicate()
             if proc.returncode != 0:
                 print('Extracting failed')
                 break
-
-            print('leaving %d valid views' % len(valid_idxs))
-            num_images = len(os.listdir(os.path.join(args.inputdir, 'scans', scene_id, 'annotation', 'segmentation')))
-            for frame_idx in range(num_images):
-                if frame_idx not in valid_idxs:
-                    color_image_path = os.path.join(args.inputdir, 'scans', scene_id, 'frames',
-                                                    'frame-%06d.color.jpg' % frame_idx)
-                    depth_image_path = os.path.join(args.inputdir, 'scans', scene_id, 'frames',
-                                                    'frame-%06d.depth.pgm' % frame_idx)
-                    pose_path = os.path.join(args.inputdir, 'scans', scene_id, 'frames',
-                                             'frame-%06d.pose.txt' % frame_idx)
-                    # print('deleting ' + color_image_path)
-                    os.remove(color_image_path)
-                    # print('deleting ' + depth_image_path)
-                    os.remove(depth_image_path)
-                    # print('deleting ' + pose_path)
-                    os.remove(pose_path)
+            #
+            # print('leaving %d valid views' % len(valid_idxs))
+            # num_images = len(os.listdir(os.path.join(args.inputdir, 'scans', scene_id, 'annotation', 'segmentation')))
+            # for frame_idx in range(num_images):
+            #     if frame_idx not in valid_idxs:
+            #         color_image_path = os.path.join(args.inputdir, 'scans', scene_id, 'frames',
+            #                                         'frame-%06d.color.jpg' % frame_idx)
+            #         depth_image_path = os.path.join(args.inputdir, 'scans', scene_id, 'frames',
+            #                                         'frame-%06d.depth.pgm' % frame_idx)
+            #         pose_path = os.path.join(args.inputdir, 'scans', scene_id, 'frames',
+            #                                  'frame-%06d.pose.txt' % frame_idx)
+            #         # print('deleting ' + color_image_path)
+            #         os.remove(color_image_path)
+            #         # print('deleting ' + depth_image_path)
+            #         os.remove(depth_image_path)
+            #         # print('deleting ' + pose_path)
+            #         os.remove(pose_path)
         else:
             print('skipping ' + 'scans/' + scene_id)
             # print('removing ' + os.path.join(args.inputdir, 'scans', scene_id, 'frames'))
