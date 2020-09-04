3ea0e33518f44a7b269e5d1a01598f19b89d1adf
jwietrzykowski <jan.m.wietrzykowski@gmail.com>
2020-09-04 08:38:50 +0200

    Testing disp features from ground truth depth.

--- data_prep/test_nms.py
+++ data_prep/test_nms.py
@@ -8,6 +8,7 @@ import sys
 import os
 import torch
 import torchvision
+import nms
 from joblib import Parallel, delayed
 from disjoint_set import DisjointSet
 
@@ -68,25 +69,33 @@ def remove_nms(anchors, scores, planes, iou_thresh=0.3, score_thresh=0.9):
     #         max_idx = i
     #         break
 
-    keep_mask = torch.full([anchors_s.shape[0]], fill_value=True, dtype=torch.bool)
-    for i in range(anchors_s.shape[0]):
-        if i % 1000 == 0:
-            print('i = ', i)
-        if keep_mask[i]:
-            for j in range(i + 1, anchors_s.shape[0]):
-                if keep_mask[j]:
-                    iou = calc_iou(anchors_s[sort_idxs[i]], anchors_s[sort_idxs[j]])
-                    if iou > iou_thresh:
-                        # norm_dot = plane_to_plane_dot(planes_s[sort_idxs[i]], planes_s[sort_idxs[j]])
-                        # if norm_dot > torch.tensor(10.0 * np.pi / 180.0).cos():
-                        #     dist = plane_to_plane_dist(planes_s[sort_idxs[i]], planes_s[sort_idxs[j]])
-                        #     if dist.abs() < 0.1:
-                        keep_mask[j] = False
+    print('pytorch nms on %d anchors' % score_idxs.shape[0])
+    # keep_torch = torchvision.ops.nms(anchors_s, (sizes_s * scores_s), iou_thresh)
+    keep_torch, num_to_keep, _ = nms.nms(anchors_s.cuda(), (sizes_s * scores_s).cuda(), overlap=iou_thresh, top_k=400)
+    keep_torch = keep_torch[:num_to_keep].cpu()
+    print('keep_torch.shape = ', keep_torch.shape)
+    keep_idxs_all = score_idxs[keep_torch]
+
+    # print('nms on %d anchors' % score_idxs.shape[0])
+    # keep_mask = torch.full([anchors_s.shape[0]], fill_value=True, dtype=torch.bool)
+    # for i in range(anchors_s.shape[0]):
+    #     if i % 1000 == 0:
+    #         print('i = ', i)
+    #     if keep_mask[i]:
+    #         for j in range(i + 1, anchors_s.shape[0]):
+    #             if keep_mask[j]:
+    #                 iou = calc_iou(anchors_s[sort_idxs[i]], anchors_s[sort_idxs[j]])
+    #                 if iou > iou_thresh:
+    #                     # norm_dot = plane_to_plane_dot(planes_s[sort_idxs[i]], planes_s[sort_idxs[j]])
+    #                     # if norm_dot > torch.tensor(10.0 * np.pi / 180.0).cos():
+    #                     #     dist = plane_to_plane_dist(planes_s[sort_idxs[i]], planes_s[sort_idxs[j]])
+    #                     #     if dist.abs() < 0.1:
+    #                     keep_mask[j] = False
 
     # nms_anchors = anchors[keep_mask]
     # nms_scores = scores[keep_mask]
-    keep_idxs = sort_idxs[keep_mask]
-    keep_idxs_all = score_idxs[keep_idxs]
+    # keep_idxs = sort_idxs[keep_mask]
+    # keep_idxs_all = score_idxs[keep_idxs]
 
     # keep_idxs_all = torch.zeros_like(keep_idxs)
     # for keep_idx in keep_idxs:
@@ -96,6 +105,31 @@ def remove_nms(anchors, scores, planes, iou_thresh=0.3, score_thresh=0.9):
 
 
 def comp_score(idx, cur_points, scores, masks, planes):
+    if idx % 1000 == 0:
+        print('idx = ', idx)
+
+    cur_points = cur_points.reshape((3, -1)).transpose(0, 1).clone()
+    valid_mask = torch.norm(cur_points, dim=-1) > 1e-3
+    valid_points = cur_points[valid_mask]
+    plane = None
+    if valid_points.shape[0] > 0:
+        mask_valid, plane = utils.fit_plane_ransac(valid_points.reshape(-1, 3))
+        num_inliers = mask_valid.sum()
+    else:
+        num_inliers = torch.zeros(1)
+    # print('num_inliners = ', num_inliers, ', num points = ', cur_points.shape[0])
+    cur_score = num_inliers.float() / cur_points.shape[0]
+    # print('cur_score = ', cur_score)
+    scores[idx] = cur_score
+    if plane is not None:
+        planes[idx] = plane
+        mask = torch.zeros(masks[idx].shape, dtype=torch.bool)
+        mask[valid_mask.reshape(masks[idx].shape)] = mask_valid
+        masks[idx][mask] = 255
+        # print(masks[idx])
+
+
+def comp_score_np(idx, cur_points, scores, masks, planes):
     if idx % 1000 == 0:
         print('idx = ', idx)
 
@@ -134,6 +168,8 @@ def test_planes(scene_id, config):
                   [0.0, 579.411, 240.0],
                   [0.0, 0.0, 1.0]], dtype=np.float)
     for im_file in images_list[0:1]:
+        print(im_file)
+
         im = cv2.imread(os.path.join(ROOT_FOLDER, scene_id, 'frames', 'color_left', im_file))
         depth = cv2.imread(os.path.join(ROOT_FOLDER, scene_id, 'frames', 'depth_left', im_file.replace('.jpg', '.png')),
                            cv2.IMREAD_ANYDEPTH) / 1000.0
@@ -151,22 +187,22 @@ def test_planes(scene_id, config):
         # Parallel(n_jobs=4, require='sharedmem')(
         #         delayed(comp_score)(idx, anchor_points[idx], scores, masks, planes) for idx in range(anchor_points.shape[0]))
 
-        for idx in range(anchor_points.shape[0]):
-            comp_score(idx, anchor_points[idx], scores, masks, planes)
+        # for idx in range(anchor_points.shape[0]):
+        #     comp_score(idx, anchor_points[idx], scores, masks, planes)
+        #
+        # with open('test/scores_' + scene_id + ' ' + im_file.replace('.jpg', '') + '.pt', 'wb') as f:
+        #     torch.save(scores, f)
+        # with open('test/planes_' + scene_id + ' ' + im_file.replace('.jpg', '') + '.pt', 'wb') as f:
+        #     torch.save(planes, f)
+        # with open('test/masks_' + scene_id + ' ' + im_file.replace('.jpg', '') + '.pt', 'wb') as f:
+        #     torch.save(masks, f)
 
-        with open('test/scores_' + scene_id + ' ' + im_file.replace('.jpg', '') + '.pt', 'wb') as f:
-            torch.save(scores, f)
-        with open('test/planes_' + scene_id + ' ' + im_file.replace('.jpg', '') + '.pt', 'wb') as f:
-            torch.save(planes, f)
-        with open('test/masks_' + scene_id + ' ' + im_file.replace('.jpg', '') + '.pt', 'wb') as f:
-            torch.save(masks, f)
-
-        # with open('test/scores_' + scene_id + ' ' + im_file.replace('.jpg', '') + '.pt', 'rb') as f:
-        #     scores = torch.load(f)
-        # with open('test/planes_' + scene_id + ' ' + im_file.replace('.jpg', '') + '.pt', 'rb') as f:
-        #     planes = torch.load(f)
-        # with open('test/masks_' + scene_id + ' ' + im_file.replace('.jpg', '') + '.pt', 'rb') as f:
-        #     masks = torch.load(f)
+        with open('test/scores_' + scene_id + ' ' + im_file.replace('.jpg', '') + '.pt', 'rb') as f:
+            scores = torch.load(f)
+        with open('test/planes_' + scene_id + ' ' + im_file.replace('.jpg', '') + '.pt', 'rb') as f:
+            planes = torch.load(f)
+        with open('test/masks_' + scene_id + ' ' + im_file.replace('.jpg', '') + '.pt', 'rb') as f:
+            masks = torch.load(f)
 
         # keep = torchvision.ops.nms(anchors, scores, 0.3)
         keep = remove_nms(anchors, scores, planes, 0.5, 0.9)
@@ -183,8 +219,8 @@ def test_planes(scene_id, config):
                     iou1 = calc_iou(anchors[keep[i]], anchors[keep[j]])
                     iou2 = calc_iou(anchors[keep[j]], anchors[keep[i]])
                     norm_dot = planes[keep[i]].dot(planes[keep[j]]) / (planes[keep[i]].norm() * planes[keep[j]].norm())
-                    # if (iou1 > merge_thresh or iou2 > merge_thresh) and norm_dot > torch.tensor(10.0 * np.pi / 180.0).cos():
-                    if (iou1 > merge_thresh or iou2 > merge_thresh):
+                    if (iou1 > merge_thresh or iou2 > merge_thresh) and norm_dot > torch.tensor(10.0 * np.pi / 180.0).cos():
+                    # if (iou1 > merge_thresh or iou2 > merge_thresh):
                         keep_ds.union(i, j)
 
         segm = np.zeros([im.shape[0], im.shape[1], 3], dtype=np.uint8)
@@ -204,7 +240,7 @@ def test_planes(scene_id, config):
             y2 = min(pt2[1], segm.shape[0])
             # print(pt1, pt2)
             segm[y1:y2, x1:x2, :] = np.tile(np.expand_dims(mask[y1 - pt1[1]: mask.shape[0] - (pt2[1] - y2), x1 - pt1[0]: mask.shape[1] - (pt2[0] - x2)], axis=-1), [1, 1, 3]) * segm_color
-            print(tuple(color_map[id]))
+            # print(tuple(color_map[id]))
             cv2.rectangle(im, (pt1[0], pt1[1]), (pt2[0], pt2[1]), (int(color_map[id,0]),int(color_map[id,1]),int(color_map[id,2])), 2)
             # if idx > 0:
             #     iou = calc_iou(anchors[keep[idx - 1]], anchors[keep[idx]])
--- models/model.py
+++ models/model.py
@@ -407,10 +407,9 @@ def pyramid_roi_align(inputs, pool_size, image_shape):
     image_area = Variable(torch.FloatTensor([float(image_shape[0]*image_shape[1])]), requires_grad=False)
     if boxes.is_cuda:
         image_area = image_area.cuda()
-    roi_level = 4 + log2(torch.sqrt(h*w)/(224.0/torch.sqrt(image_area)))
+    roi_level = 4 + log2(torch.sqrt(h*w)/(640.0/torch.sqrt(image_area)))
     roi_level = roi_level.round().int()
     roi_level = roi_level.clamp(2, 5)
-
 
     ## Loop through levels and apply ROI pooling to each. P2 to P5.
     pooled = []
@@ -1101,31 +1100,31 @@ class Mask(nn.Module):
 
 
 class Depth(nn.Module):
-    def __init__(self, num_output_channels=1):
+    def __init__(self, num_input_feats, num_output_channels=1):
         super(Depth, self).__init__()
         self.num_output_channels = num_output_channels        
         self.conv1 = nn.Sequential(
-            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(num_input_feats, 128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128, eps=0.001, momentum=0.01),
             nn.ReLU(inplace=True)
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(num_input_feats, 128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128, eps=0.001, momentum=0.01),
             nn.ReLU(inplace=True)
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(num_input_feats, 128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128, eps=0.001, momentum=0.01),
             nn.ReLU(inplace=True)
         )
         self.conv4 = nn.Sequential(
-            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(num_input_feats, 128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128, eps=0.001, momentum=0.01),
             nn.ReLU(inplace=True)
         )
         self.conv5 = nn.Sequential(
-            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(num_input_feats, 128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128, eps=0.001, momentum=0.01),
             nn.ReLU(inplace=True)
         )
@@ -1633,8 +1632,8 @@ class MaskRCNN(nn.Module):
         ## Bottom-up Layers
         ## Returns a list of the last layers of each stage, 5 in total.
         ## Don't create the thead (stage 5), so we pick the 4th item in the list.
-        # resnet = ResNet("resnet101", stage5=True, numInputChannels=config.NUM_INPUT_CHANNELS)
-        resnet = ResNet("resnet50", stage5=True, numInputChannels=config.NUM_INPUT_CHANNELS)
+        resnet = ResNet("resnet101", stage5=True, numInputChannels=config.NUM_INPUT_CHANNELS)
+        # resnet = ResNet("resnet50", stage5=True, numInputChannels=config.NUM_INPUT_CHANNELS)
         C1, C2, C3, C4, C5 = resnet.stages()
 
         ## Top-down Layers
@@ -1651,26 +1650,29 @@ class MaskRCNN(nn.Module):
             self.anchors = self.anchors.cuda()
 
         ## RPN
-        self.rpn = RPN(len(config.RPN_ANCHOR_RATIOS), config.RPN_ANCHOR_STRIDE, 256)
+        self.rpn = RPN(len(config.RPN_ANCHOR_RATIOS), config.RPN_ANCHOR_STRIDE, 256 + config.MAXDISP)
 
         ## Coordinate feature
         self.coordinates = nn.Conv2d(3, 64, kernel_size=1, stride=1)
         
         ## FPN Classifier
         self.debug = False
-        self.classifier = Classifier(256, config.POOL_SIZE, config.IMAGE_SHAPE, config.NUM_CLASSES, config.NUM_PARAMETERS, debug=self.debug)
+        self.classifier = Classifier(256 + config.MAXDISP, config.POOL_SIZE, config.IMAGE_SHAPE, config.NUM_CLASSES, config.NUM_PARAMETERS, debug=self.debug)
 
         ## FPN Mask
-        self.mask = Mask(config, 256, config.MASK_POOL_SIZE, config.IMAGE_SHAPE, config.NUM_CLASSES)
+        self.mask = Mask(config, 256 + config.MAXDISP, config.MASK_POOL_SIZE, config.IMAGE_SHAPE, config.NUM_CLASSES)
 
         if self.config.PREDICT_DEPTH:
             if self.config.PREDICT_STEREO:
-                self.depth = DepthStereo(self.config.MAXDISP, self.config.IMAGE_SHAPE[0], self.config.IMAGE_SHAPE[1], 256)
+                self.depth = DepthStereo(self.config.MAXDISP,
+                                         self.config.IMAGE_SHAPE[0],
+                                         self.config.IMAGE_SHAPE[1],
+                                         256 + config.MAXDISP)
             else:
                 if self.config.PREDICT_BOUNDARY:
-                    self.depth = Depth(num_output_channels=3)
+                    self.depth = Depth(256 + config.MAXDISP, num_output_channels=3)
                 else:
-                    self.depth = Depth(num_output_channels=1)
+                    self.depth = Depth(256 + config.MAXDISP, num_output_channels=1)
                     pass
                 pass
 
@@ -1781,13 +1783,21 @@ class MaskRCNN(nn.Module):
             except:
                 print('load only base model')
                 try:
-                    state_dict = {k: v for k, v in state_dict.items() if 'classifier.linear_class' not in k and 'classifier.linear_bbox' not in k and 'mask.conv5' not in k}
+                    state_dict = {k: v for k, v in state_dict.items() if 'classifier.linear_class' not in k
+                                  and 'classifier.linear_bbox' not in k
+                                  and 'mask.conv5' not in k}
                     state = self.state_dict()
                     state.update(state_dict)
                     self.load_state_dict(state)
                 except:
                     print('change input dimension')
-                    state_dict = {k: v for k, v in state_dict.items() if 'classifier.linear_class' not in k and 'classifier.linear_bbox' not in k and 'mask.conv5' not in k and 'fpn.C1.0' not in k and 'classifier.conv1' not in k}
+                    state_dict = {k: v for k, v in state_dict.items() if 'classifier.linear_class' not in k
+                                  and 'classifier.linear_bbox' not in k
+                                  and 'mask.conv5' not in k
+                                  and 'mask.conv1' not in k
+                                  # and 'fpn.C1.0' not in k
+                                  and 'classifier.conv1' not in k
+                                  and 'rpn.conv_shared' not in k}
                     state = self.state_dict()
                     state.update(state_dict)
                     self.load_state_dict(state)
@@ -1879,50 +1889,50 @@ class MaskRCNN(nn.Module):
         rpn_feature_maps = [p2_out, p3_out, p4_out, p5_out, p6_out]
         mrcnn_feature_maps = [p2_out, p3_out, p4_out, p5_out]
 
-        # # TODO Checking disp features
-        # gt_depth = input[7].unsqueeze(1)
-        # camera = input[6]
-        # fx = camera[0]
-        # gt_disp = fx * torch.tensor(self.config.BASELINE, dtype=torch.float, requires_grad=False).cuda() / \
-        #                 torch.clamp(gt_depth, min=1.0e-4)
-        # for stage in range(2, 7):
-        #     h = rpn_feature_maps[stage-2].shape[2]
-        #     w = rpn_feature_maps[stage-2].shape[3]
-        #
-        #     cur_disp = torch.nn.functional.interpolate(gt_disp,
-        #                                                size=(h, w),
-        #                                                mode='bilinear').view(-1, h, w, 1)
-        #     disp_vol = torch.zeros((1,
-        #                             h,
-        #                             w,
-        #                             self.config.MAXDISP),
-        #                            dtype=torch.float, requires_grad=False).cuda()
-        #
-        #     cur_disp_low = cur_disp.floor().to(torch.long)
-        #     cur_disp_high = cur_disp.ceil().to(torch.long)
-        #     mask = cur_disp_high < self.config.MAXDISP
-        #
-        #     # urange = torch.arange(w, dtype=torch.long, requires_grad=False).cuda().reshape(1, -1).repeat(h, 1)
-        #     # vrange = torch.arange(h, dtype=torch.long, requires_grad=False).cuda().reshape(-1, 1).repeat(1, w)
-        #     # ind = torch.stack([torch.zeros_like(cur_disp_low).cuda(),
-        #     #                    cur_disp_low,
-        #     #                    vrange.expand_as(cur_disp_low),
-        #     #                    urange.expand_as(cur_disp_low)],
-        #     #                   dim=-1)
-        #     mask_vol = torch.zeros_like(disp_vol, dtype=torch.bool, requires_grad=False).cuda()
-        #     mask_vol.scatter_(-1, cur_disp_low.clamp(max=self.config.MAXDISP - 1), mask)
-        #     disp_vol[mask_vol] = 1.0 - (cur_disp[mask] - cur_disp_low[mask])
-        #
-        #     mask_vol = torch.zeros_like(disp_vol, dtype=torch.bool, requires_grad=False).cuda()
-        #     mask_vol.scatter_(-1, cur_disp_high.clamp(max=self.config.MAXDISP - 1), mask)
-        #     disp_vol[mask_vol] = 1.0 - (cur_disp_high[mask] - cur_disp[mask])
-        #
-        #     # move disp dimension (3) to 1
-        #     disp_vol = disp_vol.transpose(1, 3)
-        # 
-        #     rpn_feature_maps[stage-2] = torch.cat([rpn_feature_maps[stage-2], disp_vol], dim=1)
-        #     if stage < 6:
-        #         mrcnn_feature_maps[stage-2] = torch.cat([mrcnn_feature_maps[stage-2], disp_vol], dim=1)
+        # TODO Checking disp features
+        gt_depth = input[7].unsqueeze(1)
+        camera = input[6]
+        fx = camera[0]
+        gt_disp = fx * torch.tensor(self.config.BASELINE, dtype=torch.float, requires_grad=False).cuda() / \
+                        torch.clamp(gt_depth, min=1.0e-4)
+        for stage in range(2, 7):
+            h = rpn_feature_maps[stage-2].shape[2]
+            w = rpn_feature_maps[stage-2].shape[3]
+
+            cur_disp = torch.nn.functional.interpolate(gt_disp,
+                                                       size=(h, w),
+                                                       mode='bilinear').view(-1, h, w, 1)
+            disp_vol = torch.zeros((1,
+                                    h,
+                                    w,
+                                    self.config.MAXDISP),
+                                   dtype=torch.float, requires_grad=False).cuda()
+
+            cur_disp_low = cur_disp.floor().to(torch.long)
+            cur_disp_high = cur_disp.ceil().to(torch.long)
+            mask = cur_disp_high < self.config.MAXDISP
+
+            # urange = torch.arange(w, dtype=torch.long, requires_grad=False).cuda().reshape(1, -1).repeat(h, 1)
+            # vrange = torch.arange(h, dtype=torch.long, requires_grad=False).cuda().reshape(-1, 1).repeat(1, w)
+            # ind = torch.stack([torch.zeros_like(cur_disp_low).cuda(),
+            #                    cur_disp_low,
+            #                    vrange.expand_as(cur_disp_low),
+            #                    urange.expand_as(cur_disp_low)],
+            #                   dim=-1)
+            mask_vol = torch.zeros_like(disp_vol, dtype=torch.bool, requires_grad=False).cuda()
+            mask_vol.scatter_(-1, cur_disp_low.clamp(max=self.config.MAXDISP - 1), mask)
+            disp_vol[mask_vol] = 1.0 - (cur_disp[mask] - cur_disp_low[mask])
+
+            mask_vol = torch.zeros_like(disp_vol, dtype=torch.bool, requires_grad=False).cuda()
+            mask_vol.scatter_(-1, cur_disp_high.clamp(max=self.config.MAXDISP - 1), mask)
+            disp_vol[mask_vol] = 1.0 - (cur_disp_high[mask] - cur_disp[mask])
+
+            # move disp dimension (3) to 1
+            disp_vol = disp_vol.transpose(1, 3)
+
+            rpn_feature_maps[stage-2] = torch.cat([rpn_feature_maps[stage-2], disp_vol], dim=1)
+            if stage < 6:
+                mrcnn_feature_maps[stage-2] = torch.cat([mrcnn_feature_maps[stage-2], disp_vol], dim=1)
 
         feature_maps = [feature_map for index, feature_map in enumerate(rpn_feature_maps[::-1])]
         if self.config.PREDICT_DEPTH:
